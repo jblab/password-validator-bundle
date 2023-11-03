231fc861b32b5f278821ae93133c1964b4b175bd
jbonnier <noreply@julienbonnier.com>
2023-11-03 16:04:32 -0400

    Adding validator class

--- src/JblabPasswordValidatorBundle.php
+++ src/JblabPasswordValidatorBundle.php
@@ -2,11 +2,13 @@
 
 namespace Jblab\PasswordValidatorBundle;
 
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
 /**
  * Class JblabPasswordValidatorBundle
  * @package Jblab\PasswordValidatorBundle
  */
-class JblabPasswordValidatorBundle
+class JblabPasswordValidatorBundle extends Bundle
 {
 
 }
