811393fb9b7c7caac08b0ffe0db1aaf68e039c34
jbonnier <noreply@julienbonnier.com>
2023-11-03 16:05:22 -0400

    Adding exception types

---
+++ src/Exception/PasswordSpecialCharacterException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Jblab\PasswordValidatorBundle\Exception;
+
+/**
+ * Class PasswordValidationException
+ * @package Jblab\PasswordValidatorBundle\Exception
+ */
+class PasswordSpecialCharacterException extends PasswordValidationException
+{
+}
