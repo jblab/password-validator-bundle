69448af0b43a4b7ef0b35e73b4f4e585e42ba2ad
Julien Bonnier <noreply@julienbonnier.com>
2025-02-12 13:12:35 -0500

    chore: add quality tools

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -9,6 +9,7 @@
 
 namespace Jblab\PasswordValidatorBundle\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -17,22 +18,47 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('jblab_password_validator');
-        $rootNode    = method_exists($treeBuilder, 'getRootNode')
-            ? $treeBuilder->getRootNode()
-            : $treeBuilder->root('jblab_password_validator');
+        /** @var ArrayNodeDefinition $rootNode */
+        $rootNode = $treeBuilder->getRootNode();
 
+        // @formatter:off
         $rootNode
             ->children()
-            ->integerNode('minimum_length')->defaultValue(8)->info('Minimum password length.')->end()
-            ->integerNode('maximum_length')->defaultValue(64)->info('Maximum password length.')->end()
-            ->booleanNode('require_special_character')->defaultTrue()->info('Whether or not to require a special character.')->end()
-            ->booleanNode('require_uppercase')->defaultTrue()->info('Whether or not to require a uppercase letter.')->end()
-            ->booleanNode('require_lowercase')->defaultTrue()->info('Whether or not to require a lowercase letter.')->end()
-            ->booleanNode('require_number')->defaultTrue()->info('Whether or not to require a number.')->end()
-            ->scalarNode('special_character_set')->defaultValue('!@#$%^&*()_+-=[]{}|\'')->info('String containing all valid special characters')->end()
-            ->scalarNode('excluded_character_set')->defaultNull()->info('String containing all invalid characters')->end()
+                ->integerNode('minimum_length')
+                    ->defaultValue(8)
+                    ->info('Minimum password length.')
+                ->end()
+                ->integerNode('maximum_length')
+                    ->defaultValue(64)
+                    ->info('Maximum password length.')
+                ->end()
+                ->booleanNode('require_special_character')
+                    ->defaultTrue()
+                    ->info('Whether or not to require a special character.')
+                ->end()
+                ->booleanNode('require_uppercase')
+                    ->defaultTrue()
+                    ->info('Whether or not to require a uppercase letter.')
+                ->end()
+                ->booleanNode('require_lowercase')
+                    ->defaultTrue()
+                    ->info('Whether or not to require a lowercase letter.')
+                ->end()
+                ->booleanNode('require_number')
+                    ->defaultTrue()
+                    ->info('Whether or not to require a number.')
+                ->end()
+                ->scalarNode('special_character_set')
+                    ->defaultValue('!@#$%^&*()_+-=[]{}|\'')
+                    ->info('String containing all valid special characters')
+                ->end()
+                ->scalarNode('excluded_character_set')
+                    ->defaultNull()
+                    ->info('String containing all invalid characters')
+                ->end()
             ->end()
         ;
+        // @formatter:on
 
         return $treeBuilder;
     }
--- src/PasswordValidatorInterface.php
+++ src/PasswordValidatorInterface.php
@@ -9,7 +9,28 @@
 
 namespace Jblab\PasswordValidatorBundle;
 
+use Exception;
+use Jblab\PasswordValidatorBundle\Exception\PasswordExcludedCharacterException;
+use Jblab\PasswordValidatorBundle\Exception\PasswordLowercaseException;
+use Jblab\PasswordValidatorBundle\Exception\PasswordMaximumLengthException;
+use Jblab\PasswordValidatorBundle\Exception\PasswordMinimumLengthException;
+use Jblab\PasswordValidatorBundle\Exception\PasswordNumberException;
+use Jblab\PasswordValidatorBundle\Exception\PasswordSpecialCharacterException;
+use Jblab\PasswordValidatorBundle\Exception\PasswordUppercaseException;
+use Jblab\PasswordValidatorBundle\Exception\PasswordValidationException;
+
 interface PasswordValidatorInterface
 {
-    public function validate(string $password, bool $throwException = false);
+    /**
+     * @throws PasswordExcludedCharacterException
+     * @throws PasswordLowercaseException
+     * @throws PasswordMaximumLengthException
+     * @throws PasswordMinimumLengthException
+     * @throws PasswordNumberException
+     * @throws PasswordSpecialCharacterException
+     * @throws PasswordUppercaseException
+     * @throws PasswordValidationException
+     * @throws Exception
+     */
+    public function validate(string $password, bool $throwException = false): bool;
 }
